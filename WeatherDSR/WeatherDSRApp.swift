c2e9c199c857b18ac17c766cc355a9c37f70262c
Gleb K <gleb.kapustin.98@mail.ru>
2024-05-28 13:52:51 +0300

    added swiftlint

---
+++ WeatherDSR/WeatherDSRApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDSRApp.swift
+//  WeatherDSR
+//
+//  Created by Глеб Капустин on 28.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDSRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
